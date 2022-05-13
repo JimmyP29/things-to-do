d54e0cddd151f3464ef0335ac820185b91338479
James Privett <james.privett@feefo.com>
2022-05-13 14:31:13 +0100

    Server setup

---
+++ index.js
@@ -0,0 +1,12 @@
+const { ApolloServer } = require('apollo-server-express');
+const express = require('express');
+
+const app = express();
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.applyMiddleware({ app });
+
+const PORT = 3001;
+
+app.listen({ port: PORT }, () => { console.log(`Running on port ${PORT}, let's go!!!`); });
