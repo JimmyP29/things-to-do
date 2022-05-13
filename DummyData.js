9699db8f80ed1b6a61f4e7eb6fb689674a5612b8
James Privett <james.privett@feefo.com>
2022-05-13 14:23:25 +0100

    Dummy data

---
+++ DummyData.js
@@ -0,0 +1,19 @@
+let todos = [
+    {
+        id: 1,
+        description: 'Make cup of coffee',
+        isCompleted: true,
+    },
+    {
+        id: 2,
+        description: 'Review PRs from yesterday',
+        isCompleted: false,
+    },
+    {
+        id: 3,
+        description: 'Annoy Belmiro',
+        isCompleted: false,
+    }
+];
+
+module.exports = { todos };
